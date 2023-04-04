4379580990b3b9ec11257cb0219932dff988e37b
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-04-04 14:54:08 -0400

    Use inquirer to prompt the user for input

--- index.js
+++ index.js
@@ -29,5 +29,32 @@ const createSvg = (text, textColor, shape, shapeColor) => {
   
 };
 
-
+// Use inquirer to prompt the user for input
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Enter up to three characters:",
+      // Validate the input to ensure it's 3 characters or less
+      validate: (input) => input.length <= 3,
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter the text color (keyword or hexadecimal number):",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose a shape:",
+      // Provide a list of shape choices
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Enter the shape's color (keyword or hexadecimal number):",
+    },
+  ])
     
