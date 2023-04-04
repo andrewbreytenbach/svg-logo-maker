fbd87b56b6bdf16ec277c347137baa6643b8d0f2
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-04-04 16:20:52 -0400

    had to add the recommended starter framework

--- index.js
+++ index.js
@@ -1,69 +1,64 @@
-// This will import the required libraries for the app to run correctly
+// Import required modules
 const inquirer = require("inquirer");
-const chalk = require("chalk");
 const fs = require("fs");
+const { Triangle, Circle, Square } = require("./lib/shapes");
 
-// Function to create the SVG content and save it to a file
-const createSvg = (text, textColor, shape, shapeColor) => {
-    // Define the SVG content using a template literal
-    // This includes the specified shape, text, and colors
-    const svgContent = `
-  <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-    <${shape} fill="${shapeColor}" ${
-      // Depending on the chosen shape, add appropriate SVG attributes
-      shape === "circle"
-        ? 'cx="150" cy="100" r="50"'
-        : shape === "triangle"
-        ? 'points="150,50 100,150 200,150"'
-        : 'x="100" y="50" width="100" height="100"'
-    }/>
-    <text x="150" y="100" text-anchor="middle" dy=".3em" font-size="48" fill="${textColor}">${text}</text>
-  </svg>
-    `;
-
-    // Write the SVG content to a file named logo.svg
-  fs.writeFileSync("logo.svg", svgContent);
-
-  // Print a success message to the console using the chalk library for colored text
-  console.log(chalk.green("Generated logo.svg"));
-  
-};
-
-// Use inquirer to prompt the user for input
-inquirer
-  .prompt([
+// Main function to run the application
+async function main() {
+  // Collect user input using inquirer
+  const answers = await inquirer.prompt([
     {
       type: "input",
       name: "text",
-      message: "Enter up to three characters:",
-      // Validate the input to ensure it's 3 characters or less
-      validate: (input) => input.length <= 3,
+      message: "Enter up to three characters for the logo:",
+      validate: (input) => {
+        return input.length <= 3 || "Please enter no more than 3 characters.";
+      },
     },
     {
       type: "input",
       name: "textColor",
-      message: "Enter the text color (keyword or hexadecimal number):",
+      message: "Enter the text color (keyword or hex value):",
     },
     {
       type: "list",
       name: "shape",
-      message: "Choose a shape:",
-      // Provide a list of shape choices
+      message: "Choose a shape for the logo:",
       choices: ["circle", "triangle", "square"],
     },
     {
       type: "input",
       name: "shapeColor",
-      message: "Enter the shape's color (keyword or hexadecimal number):",
+      message: "Enter the shape color (keyword or hex value):",
     },
-  ])
+  ]);
 
-// When the user finishes providing input, call the createSvg function with the answers
-.then((answers) => {
-createSvg(answers.text, answers.textColor, answers.shape, answers.shapeColor);
-})
+  // Create a new shape instance based on user input
+  let shape;
+  switch (answers.shape) {
+    case "circle":
+      shape = new Circle();
+      break;
+    case "triangle":
+      shape = new Triangle();
+      break;
+    case "square":
+      shape = new Square();
+      break;
+  }
 
- // If there's an error during the process, print an error message to the console
- .catch((error) => {
-    console.error(chalk.red("Error generating logo:", error));
-  });
+  // Set the shape color
+  shape.setColor(answers.shapeColor);
+
+  // Generate the logo SVG string
+  const logoSVG = shape.renderWithText(answers.text, answers.textColor);
+
+  // Write the logo SVG to a file
+  fs.writeFileSync("logo.svg", logoSVG);
+
+  // Notify the user that the file has been generated
+  console.log("Generated logo.svg");
+}
+
+// Run the main function
+main();
