442e85531fd4693c2085a7dc414d1afcdb2ef657
Andrew Breytenbach <ambreyts15@gmail.com>
2023-04-12 14:38:35 -0400

    got the jest class to work

---
+++ shapes.test.js
@@ -0,0 +1,11 @@
+const { Triangle } = require('./shapes.cjs');
+
+test("Triangle should render correct SVG element", () => {
+  const shape = new Triangle();
+  shape.setColor("blue");
+  expect(shape.render()).toEqual(
+    '<polygon points="150,18 244,182 56,182" fill="blue" />'
+  );
+});
+
+
